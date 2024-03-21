668f8d091848b488bd6a524465a93b74280cc780
JasonGROSSO <114397870+JasonGROSSO@users.noreply.github.com>
2024-03-21 10:33:27 +0100

    create the game window

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,19 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(1000, 800), "Super Chicken Boy!");
+    window.setFramerateLimit(60);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
+    return 0;
+}
