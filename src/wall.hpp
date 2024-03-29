f5c25d5d701a8249d2e3b1142a68a219b20d179a
Guillaume DERAMCHI <guillaume.deramchi@algosup.com>
2024-03-29 09:20:40 +0100

    Update wall

--- src/wall.hpp
+++ src/wall.hpp
@@ -1,21 +1,18 @@
+#include <SFML/Graphics.hpp>
+
 class Wall {
 private:
-    int x, y; // Position of the wall
-    int width, height; // Dimensions of the wall
+    sf::RectangleShape shape;
 
 public:
-    // Constructor
-    Wall(int x, int y, int width, int height) : x(x), y(y), width(width), height(height) {}
+    Wall(sf::Vector2f position, sf::Vector2f size) {
+        shape.setPosition(position);
+        shape.setSize(size);
+        shape.setFillColor(sf::Color::White); // Change this to the color you want
+    }
 
-    // Getters
-    int getX() const { return x; }
-    int getY() const { return y; }
-    int getWidth() const { return width; }
-    int getHeight() const { return height; }
-
-    // Setters
-    void setX(int x) { this->x = x; }
-    void setY(int y) { this->y = y; }
-    void setWidth(int width) { this->width = width; }
-    void setHeight(int height) { this->height = height; }
+    void draw(sf::RenderWindow& window) {
+        window.draw(shape);
+    }
 };
+
