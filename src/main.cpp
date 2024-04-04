9469b4b0a8716515886e8d0ed4431a1e8fc3b4c4
Evan-UHRING <146000775+Evan-UHRING@users.noreply.github.com>
2024-04-04 13:15:58 +0200

    added more walls

--- src/main.cpp
+++ src/main.cpp
@@ -12,16 +12,12 @@ int main() {
     const int WINDOW_HEIGHT = GRID_SIZE * 50;
     RenderWindow window(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Super Chicken Boy!");
 
-    Player player(Vector2f(600, 600), Vector2f(GRID_SIZE - 5, GRID_SIZE - 5), 0.035f, 30.f);
+    Player player(Vector2f(GRID_SIZE * 4, GRID_SIZE * 48), Vector2f(GRID_SIZE - 5, GRID_SIZE - 5), 0.035f, 30.f);
     Vector2f velocity;
     FloatRect nextPos;
 
     Walls wall;
     wall.setWalls();
-
-    //std::vector<RectangleShape> walls1; Wall wall1(Vector2f(0, GRID_SIZE * 49), Vector2f(GRID_SIZE * 30, GRID_SIZE)); walls1.push_back(wall1.shape);
-    //std::vector<RectangleShape> walls2; Wall wall2(Vector2f(0, GRID_SIZE * 45), Vector2f(GRID_SIZE * 22, GRID_SIZE * 2)); walls2.push_back(wall2.shape);
-    //std::vector<RectangleShape> walls3; Wall wall3(Vector2f(GRID_SIZE * 29, GRID_SIZE * 37), Vector2f(GRID_SIZE, GRID_SIZE * 12)); walls3.push_back(wall3.shape);
 
     while (window.isOpen()) {
         Event event;
@@ -55,23 +51,36 @@ int main() {
             nextPos.left += velocity.x;
 
             if (wallBounds.intersects(nextPos)) {
-                switch (collisions(playerBounds, wallBounds)) {
-                    case 1:
-                        velocity.y = 0.f;
-                        player.shape.setPosition(playerBounds.left, wallBounds.top + wallBounds.height);
-                        break;
-                    case 2:
-                        velocity.y = 0.f;
-                        player.shape.setPosition(playerBounds.left, wallBounds.top - playerBounds.height);
-                        break;
-                    case 3:
-                        velocity.x = 0.f;
-                        player.shape.setPosition(wallBounds.left + wallBounds.width, playerBounds.top);
-                        break;
-                    case 4:
-                        velocity.x = 0.f;
-                        player.shape.setPosition(wallBounds.left - playerBounds.width, playerBounds.top);
-                        break;
+                if (playerBounds.top > wallBounds.top
+                && playerBounds.top + playerBounds.height > wallBounds.top + wallBounds.height
+                && playerBounds.left < wallBounds.left + wallBounds.width
+                && playerBounds.left + playerBounds.width > wallBounds.left) {
+                    velocity.y = 0.f;
+                    player.shape.setPosition(playerBounds.left, wallBounds.top + wallBounds.height);
+                }
+                // bottom
+                else if (playerBounds.top < wallBounds.top
+                && playerBounds.top + playerBounds.height < wallBounds.top + wallBounds.height
+                && playerBounds.left < wallBounds.left + wallBounds.width
+                && playerBounds.left + playerBounds.width > wallBounds.left) {
+                    velocity.y = 0.f;
+                    player.shape.setPosition(playerBounds.left, wallBounds.top - playerBounds.height);
+                }
+                // left
+                else if (playerBounds.left > wallBounds.left
+                && playerBounds.left + playerBounds.width > wallBounds.left + wallBounds.width
+                && playerBounds.top < wallBounds.top + wallBounds.height
+                && playerBounds.top + playerBounds.height > wallBounds.top) {
+                    velocity.x = 0.f;
+                    player.shape.setPosition(wallBounds.left + wallBounds.width, playerBounds.top);
+                }
+                // right
+                else if (playerBounds.left < wallBounds.left
+                && playerBounds.left + playerBounds.width < wallBounds.left + wallBounds.width
+                && playerBounds.top < wallBounds.top + wallBounds.height
+                && playerBounds.top + playerBounds.height > wallBounds.top) {
+                    velocity.x = 0.f;
+                    player.shape.setPosition(wallBounds.left - playerBounds.width, playerBounds.top);
                 }
             }
         }
@@ -96,8 +105,10 @@ int main() {
             player.shape.setPosition(WINDOW_WIDTH - player.shape.getGlobalBounds().width, player.shape.getPosition().y);
         }
 
+        // gravity
         float displacement = 0.075;
         player.shape.move(0, displacement);
+
 
         // display everything
         window.clear();
@@ -111,35 +122,4 @@ int main() {
     }
 
     return 0;
-}
-
-int collisions(FloatRect playerBounds, FloatRect wallBounds) {
-    // top
-    if (playerBounds.top > wallBounds.top
-    && playerBounds.top + playerBounds.height > wallBounds.top + wallBounds.height
-    && playerBounds.left < wallBounds.left + wallBounds.width
-    && playerBounds.left + playerBounds.width > wallBounds.left) {
-        return 1;
-    }
-    // bottom
-    else if (playerBounds.top < wallBounds.top
-    && playerBounds.top + playerBounds.height < wallBounds.top + wallBounds.height
-    && playerBounds.left < wallBounds.left + wallBounds.width
-    && playerBounds.left + playerBounds.width > wallBounds.left) {
-        return 2;
-    }
-    // left
-    else if (playerBounds.left > wallBounds.left
-    && playerBounds.left + playerBounds.width > wallBounds.left + wallBounds.width
-    && playerBounds.top < wallBounds.top + wallBounds.height
-    && playerBounds.top + playerBounds.height > wallBounds.top) {
-        return 3;
-    }
-    // right
-    else if (playerBounds.left < wallBounds.left
-    && playerBounds.left + playerBounds.width < wallBounds.left + wallBounds.width
-    && playerBounds.top < wallBounds.top + wallBounds.height
-    && playerBounds.top + playerBounds.height > wallBounds.top) {
-        return 4;
-    }
 }
--- src/wall.hpp
+++ src/wall.hpp
@@ -1,17 +1,3 @@
-//class Wall {
-//public:
-//    sf::RectangleShape shape;
-//
-//    Wall(sf::Vector2f position, sf::Vector2f size) {
-//        shape.setPosition(position);
-//        shape.setSize(size);
-//        shape.setFillColor(sf::Color{0x6f6f6fff}); // Change this to the color you want
-//    }
-//
-//    void draw(sf::RenderWindow& window) {
-//        window.draw(shape);
-//    }
-//};
 const int GRID_SIZE = 20;
 
 class Walls {
@@ -20,9 +6,15 @@ public:
     sf::RectangleShape shape;
 
     void setWalls() {
-        shape.setFillColor(sf::Color{0x6f6f6fff});
+        shape.setFillColor(sf::Color(0x6f6f6fff));
         shape.setPosition(sf::Vector2f(0, GRID_SIZE * 49)); shape.setSize(sf::Vector2f(GRID_SIZE * 30, GRID_SIZE)); walls.push_back(shape);
         shape.setPosition(sf::Vector2f(0, GRID_SIZE * 45)); shape.setSize(sf::Vector2f(GRID_SIZE * 22, GRID_SIZE * 2)); walls.push_back(shape);
-        shape.setPosition(sf::Vector2f(GRID_SIZE * 29, GRID_SIZE * 37)); shape.setSize(sf::Vector2f(GRID_SIZE, GRID_SIZE * 12)); walls.push_back(shape);
+        shape.setPosition(sf::Vector2f(GRID_SIZE * 29, GRID_SIZE * 30)); shape.setSize(sf::Vector2f(GRID_SIZE, GRID_SIZE * 19)); walls.push_back(shape);
+        shape.setPosition(sf::Vector2f(0, 0)); shape.setSize(sf::Vector2f(GRID_SIZE, GRID_SIZE * 45)); walls.push_back(shape);
+        shape.setPosition(sf::Vector2f(0, 0)); shape.setSize(sf::Vector2f(GRID_SIZE * 30, GRID_SIZE)); walls.push_back(shape);
+        shape.setPosition(sf::Vector2f(GRID_SIZE * 3, GRID_SIZE * 2)); shape.setSize(sf::Vector2f(GRID_SIZE, GRID_SIZE * 41)); walls.push_back(shape);
+        shape.setPosition(sf::Vector2f(GRID_SIZE * 3, GRID_SIZE * 40)); shape.setSize(sf::Vector2f(GRID_SIZE * 19, GRID_SIZE)); walls.push_back(shape);
+        shape.setPosition(sf::Vector2f(GRID_SIZE * 19, GRID_SIZE * 41)); shape.setSize(sf::Vector2f(GRID_SIZE * 3, GRID_SIZE * 4)); walls.push_back(shape);
+        shape.setPosition(sf::Vector2f(GRID_SIZE * 6, GRID_SIZE * 30)); shape.setSize(sf::Vector2f(GRID_SIZE * 24, GRID_SIZE)); walls.push_back(shape);
     }
 };
